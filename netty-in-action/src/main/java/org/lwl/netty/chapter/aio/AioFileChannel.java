5003f5ffc12c1d0c7bdf2ef6688e9ebf5aac57f9
yiyangtry <544130270@qq.com>
2022-06-26 17:52:13 +0800

    aio

---
+++ netty-in-action/src/main/java/org/lwl/netty/chapter/aio/AioFileChannel.java
@@ -0,0 +1,41 @@
+package org.lwl.netty.chapter.aio;
+
+import lombok.extern.slf4j.Slf4j;
+import utils.ByteBufferUtil;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousFileChannel;
+import java.nio.channels.CompletionHandler;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+@Slf4j
+public class AioFileChannel {
+    public static void main(String[] args) throws IOException {
+        try (AsynchronousFileChannel channel = AsynchronousFileChannel.open(Paths.get("data.txt"), StandardOpenOption.READ)) {
+            // 参数1 ByteBuffer
+            // 参数2 读取的起始位置
+            // 参数3 附件
+            // 参数4 回调对象 CompletionHandler
+            ByteBuffer buffer = ByteBuffer.allocate(16);
+            log.debug("read begin...");
+            channel.read(buffer, 0, buffer, new CompletionHandler<Integer, ByteBuffer>() {
+                @Override // read 成功
+                public void completed(Integer result, ByteBuffer attachment) {
+                    log.debug("read completed...{}", result);
+                    attachment.flip();
+                    ByteBufferUtil.debugAll(attachment);
+                }
+                @Override // read 失败
+                public void failed(Throwable exc, ByteBuffer attachment) {
+                    exc.printStackTrace();
+                }
+            });
+            log.debug("read end...");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.in.read();
+    }
+}
